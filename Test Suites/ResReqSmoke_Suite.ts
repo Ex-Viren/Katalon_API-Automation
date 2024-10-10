07dac920146b41213a6767c53f620c5dbf0f42c9
Viren Chauhan <viren.chauhan@expresspros.com>
2024-10-10 19:06:48 +0530

    Basic GET method script

---
+++ Test Suites/ResReqSmoke_Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ResReqSmoke_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9780be6c-c5dd-4977-b58e-9d5978fa5ca3</testSuiteGuid>
+   <testCaseLink>
+      <guid>ce5157e9-2813-4c34-99ac-37b49a843eaf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ReqResSmokeTests/Test_ListUsers</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
